0d8506182ab25de4e1a378b0bdbc0710774841e9
yasuyuky <yasuyuki.ymd@gmail.com>
2018-07-16 14:09:18 +0900

    Decaffenate kill-ring-spec

---
+++ spec/kill-ring-spec.js
@@ -0,0 +1,10 @@
+/* global describe:true it:true expect:true */
+const KillRing = require('../lib/kill-ring');
+
+describe('Kill Ring', ()=> {
+  it('should be ["text"]', ()=> {
+    let killring = new KillRing(4);
+    killring.push(['text']);
+    expect(killring.top()[0]).toBe('text');
+  });
+});
