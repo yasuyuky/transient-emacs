8583ca9122d9df36688754fe4bdcfe7b7a09a69e
yasuyuky <yasuyuki.ymd@gmail.com>
2019-01-27 14:37:43 +0900

    Beautify by prettier

--- spec/kill-ring-spec.js
+++ spec/kill-ring-spec.js
@@ -1,9 +1,9 @@
 /* global describe it expect atom:true */
 const KillRing = require('../lib/kill-ring');
 
-describe('Kill Ring', ()=> {
-  it('should be ["text"]', ()=> {
-    atom.config.set('transient-emacs', {killRing:{persistent:true, length:10}});
+describe('Kill Ring', () => {
+  it('should be ["text"]', () => {
+    atom.config.set('transient-emacs', { killRing: { persistent: true, length: 10 } });
     let killring = new KillRing([]);
     killring.push(['text']);
     expect(killring.top()[0]).toBe('text');
