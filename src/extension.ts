7c51ea6ca7df37a0230fcd53800ed1c3a2feed7d
yasuyuky <yasuyuki.ymd@gmail.com>
2019-03-25 22:59:09 +0900

    Add code for vscode

---
+++ src/extension.ts
@@ -0,0 +1,163 @@
+import * as vscode from 'vscode';
+import { KillRing } from './kill-ring';
+
+var markSet: boolean = false;
+var isUserCommand: boolean = true;
+var killRing: KillRing = new KillRing([]);
+
+export function activate(context: vscode.ExtensionContext) {
+  let commands = new Map<string, (...args: any[]) => any>([
+    ['transient.cancel', cancel],
+    ['transient.setMark', setMark],
+    ['transient.unsetMark', unsetMark],
+    ['transient.yank', yank],
+    ['transient.kill', kill],
+    ['transient.killRegion', killRegion],
+    ['transient.killRegionOrBackwardWord', killRegionOrBackwardWord],
+    ['transient.killBackwardWord', killBackwardWord],
+    ['transient.copyRegion', copyRegion]
+  ]);
+  commands.forEach((func, key) =>
+    context.subscriptions.push(vscode.commands.registerTextEditorCommand(key, func))
+  );
+
+  let moves = [
+    'cursorLeft',
+    'cursorRight',
+    'cursorUp',
+    'cursorDown',
+    'cursorHome',
+    'cursorEnd',
+    'cursorTop',
+    'cursorBottom',
+    'cursorWordLeft',
+    'cursorWordRight'
+  ];
+
+  moves.forEach(move => {
+    let key = 'transient.' + move;
+    let f = () => vscode.commands.executeCommand(markSet ? move + 'Select' : move);
+    let command = vscode.commands.registerTextEditorCommand(key, f);
+    context.subscriptions.push(command);
+  });
+
+  vscode.window.onDidChangeTextEditorSelection(() => {
+    if (isUserCommand) killRing.seal();
+  });
+}
+
+export function deactivate() {}
+
+function cancel(editor: vscode.TextEditor) {
+  if (markSet) {
+    clearSelections(editor);
+    unsetMark(editor);
+    return;
+  }
+  if (clearSelections(editor)) return;
+  if (consolidateSelections(editor)) return;
+}
+
+function clearSelections(editor: vscode.TextEditor): boolean {
+  if (editor.selections.some(s => !s.isEmpty)) {
+    editor.selections = editor.selections.map(s => new vscode.Selection(s.active, s.active));
+    return true;
+  }
+  return false;
+}
+
+function consolidateSelections(editor: vscode.TextEditor): boolean {
+  if (editor.selections.length > 1) {
+    editor.selections = [editor.selection];
+    return true;
+  }
+  return false;
+}
+
+function setMark(editor: vscode.TextEditor) {
+  console.log('set mark');
+  markSet = true;
+}
+
+function unsetMark(editor: vscode.TextEditor) {
+  console.log('unset mark');
+  markSet = false;
+}
+
+function killRegionOrBackwardWord(editor: vscode.TextEditor) {
+  if (editor.selection.isEmpty) killBackwardWord(editor);
+  else killRegion(editor);
+}
+
+function pushRegionToKillring(editor: vscode.TextEditor) {
+  markSet = false;
+  const texts = editor.selections.map(s => editor.document.getText(s));
+  killRing.push(texts);
+  killRing.seal();
+}
+
+function killRegion(editor: vscode.TextEditor) {
+  pushRegionToKillring(editor);
+  editor.edit(edit => editor.selections.forEach(s => (s.isEmpty ? false : edit.delete(s))));
+}
+
+function copyRegion(editor: vscode.TextEditor) {
+  pushRegionToKillring(editor);
+  editor.selections.forEach(s => s.with(s.start, s.start));
+}
+
+function killBackwardWord(editor: vscode.TextEditor) {
+  isUserCommand = false;
+  editor
+    .edit(edit => {
+      editor.selections = editor.selections.map(s => {
+        let wordRange = editor.document.getWordRangeAtPosition(s.active);
+        if (wordRange) return new vscode.Selection(wordRange.start, s.active);
+        const delimExp = /[`~!\@@#\%\^\&*()-\=+{}\|\;\:\'\"\,.\<>\/\?\s]+/g;
+        let delimRange = editor.document.getWordRangeAtPosition(s.active, delimExp);
+        if (delimRange) return new vscode.Selection(delimRange.start, s.active);
+        return s;
+      });
+      const texts = editor.selections.map(s => editor.document.getText(s));
+      killRing.put(texts, false);
+      editor.selections.forEach(s => (s.isEmpty ? false : edit.delete(s)));
+    })
+    .then(() => {
+      isUserCommand = true;
+    });
+}
+
+function kill(editor: vscode.TextEditor) {
+  isUserCommand = false;
+  editor
+    .edit(edit => {
+      editor.selections = editor.selections.map(s => {
+        let line = editor.document.lineAt(s.active);
+        if (line.isEmptyOrWhitespace)
+          return new vscode.Selection(s.active, line.rangeIncludingLineBreak.end);
+        return new vscode.Selection(s.active, line.range.end);
+      });
+      const texts = editor.selections.map(s => editor.document.getText(s));
+      killRing.put(texts, true);
+      editor.selections.forEach(s => (s.isEmpty ? false : edit.delete(s)));
+    })
+    .then(() => {
+      isUserCommand = true;
+    });
+}
+
+function yank(editor: vscode.TextEditor) {
+  yankTexts(editor, killRing.top());
+}
+
+function yankTexts(editor: vscode.TextEditor, texts: Array<string>) {
+  editor
+    .edit(edit => {
+      const sels = editor.selections;
+      if (texts && sels.length === texts.length) sels.forEach((s, i) => edit.replace(s, texts[i]));
+      else if (texts) sels.forEach(s => edit.replace(s, texts.join('\n')));
+    })
+    .then(() => {
+      editor.selections = editor.selections.map(s => new vscode.Selection(s.active, s.active));
+    });
+}
