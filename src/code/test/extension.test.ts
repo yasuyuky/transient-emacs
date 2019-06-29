e0168fe7c95146d0eba2e97db9ceb92f5d73b3bb
yasuyuky <yasuyuki.ymd@gmail.com>
2019-06-29 20:55:53 +0900

    Add the first test for code

---
+++ src/code/test/extension.test.ts
@@ -0,0 +1,41 @@
+//
+// Note: This example test is leveraging the Mocha test framework.
+// Please refer to their documentation on https://mochajs.org/ for help.
+//
+
+// The module 'assert' provides assertion methods from node
+import * as assert from 'assert';
+
+// You can import and use all API from the 'vscode' module
+// as well as import your extension to test it
+import * as vscode from 'vscode';
+import { Position, Selection } from 'vscode';
+
+async function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function move(editor: vscode.TextEditor, ps: Position[]) {
+  editor.selections = ps.map(p => new Selection(p, p));
+}
+
+// Defines a Mocha test suite to group tests of similar kind together
+suite('Extension Tests', async () => {
+  // Defines a Mocha unit test
+  await test('transient.kill', async () => {
+    let editor = vscode.window.activeTextEditor!;
+    await editor
+      .edit(edit => {
+        edit.insert(new Position(0, 0), 'foo bar\nbaz\n');
+      })
+      .then(async () => {
+        move(editor, [new Position(0, 0)]);
+        await vscode.commands.executeCommand('transient.kill');
+        await sleep(50);
+        assert.equal('\nbaz\n', editor.document.getText());
+        await vscode.commands.executeCommand('transient.kill');
+        await sleep(50);
+        assert.equal('baz\n', editor.document.getText());
+      });
+  });
+});
