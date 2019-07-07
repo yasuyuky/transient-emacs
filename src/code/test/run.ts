3065bc1e3922c5a8a8aa3e2615a3022c3d8a33ac
yasuyuky <yasuyuki.ymd@gmail.com>
2019-07-07 21:43:04 +0900

    Add test runner

---
+++ src/code/test/run.ts
@@ -0,0 +1,11 @@
+import * as path from 'path';
+import { runTests } from 'vscode-test';
+
+async function go() {
+  const extensionPath = path.resolve(__dirname, '../../..');
+  const testRunnerPath = path.resolve(__dirname, './');
+  const testWorkspace = path.resolve(__dirname, '../../..');
+  await runTests({ extensionPath, testRunnerPath, testWorkspace });
+}
+
+go();
