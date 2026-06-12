b18c5b13519a2a6e0a1e33cf401d31ea2a8f17a6
yasuyuky <yasuyuki.ymd@gmail.com>
2026-06-12 21:31:58 +0900

    revert test runner close

--- src/code/test/index.ts
+++ src/code/test/index.ts
@@ -1,7 +1,6 @@
 import * as path from 'path';
 import Mocha = require('mocha');
 import * as glob from 'glob';
-import * as vscode from 'vscode';
 
 export function run(): Promise<void> {
   // Create the mocha test
@@ -18,9 +17,12 @@ export function run(): Promise<void> {
 
     try {
       // Run the mocha test
-      mocha.run(async (failures: number) => {
-        await vscode.commands.executeCommand('workbench.action.closeWindow');
-        failures > 0 ? e(new Error(`${failures} tests failed.`)) : c();
+      mocha.run((failures: number) => {
+        if (failures > 0) {
+          e(new Error(`${failures} tests failed.`));
+        } else {
+          c();
+        }
       });
     } catch (err) {
       e(err);
